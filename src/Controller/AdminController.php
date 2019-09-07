aab47558f17968f7101d4c21ac322127dbd086e8
ryan <ryan_03@hotmail.fr>
2019-09-08 00:45:11 +0200

    crud user admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,12 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\CardRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AdminController extends FOSRestController
 {
@@ -24,7 +29,8 @@ class AdminController extends FOSRestController
     /**
      * @Rest\Get("/api/admin/users")
      */
-    public function getApiUsers(){
+    public function getApiUsers()
+    {
         $users = $this->userRepository->findAll();
         return $this->view($users);
     }
@@ -32,7 +38,8 @@ class AdminController extends FOSRestController
     /**
      * @Rest\Get("/api/admin/user/{id}")
      */
-    public function getApiUser(User $user){
+    public function getApiUser(User $user)
+    {
         return $this->view($user);
     }
 
@@ -45,6 +52,58 @@ class AdminController extends FOSRestController
         $this->em->persist($user);
         $this->em->flush();
         return $this->view($user);
+    }
+
+    /**
+     * @Rest\Patch("/api/admin/user/{email}")
+     */
+    public function patchApiUser(User $user, Request $request, ValidatorInterface $validator)
+    {
+        // Normalement, le tableau d'attributs ne se ferait pas ici. On opterait plutôt
+        // pour la création d'un Provider qui fournirait à l'application le tableau des attributs
+        $attributes = [
+            'firstname' => 'setFirstname',
+            'lastname' => 'setLastname',
+            'email' => 'setEmail'
+        ];
+        foreach ($attributes as $attributeName => $setterName) {
+            if (is_null($request->get($attributeName))) {
+                continue;
+            }
+            $user->$setterName($request->request->get($attributeName));
+        }
+
+        $validationErrors = $validator->validate($user);
+        if ($validationErrors->count() > 0){
+            /** @var ConstraintViolation $constraintViolation */
+            foreach ($validationErrors as $constraintViolation){
+                $message = $constraintViolation->getMessage();
+                $propertyPath = $constraintViolation->getPropertyPath();
+                $errors[] = ['message' => $message, 'propertyPath' => $propertyPath];
+            }
+        }
+
+        if (!empty($errors)){
+            //throw new BadRequestHttpException(json_encode($errors));
+        }
+        $this->em->flush();
+        return $this->view($user);
+    }
+
+    /**
+     * @Rest\Delete("/api/admin/user/{id}")
+     */
+    public function deleteApiUser(User $user)
+    {
+        $cards = $user->getCards();
+        foreach ($cards as $card)
+        {
+            $this->em->remove($card);
+        }
+
+        $this->em->remove($user);
+        $this->em->flush();
+        return new Response(null, 204);
     }
 
 
